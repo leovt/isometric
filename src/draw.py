e97c1c603772678a377ed5c2f118c53b05e777df
leovt <leonhard.vogt@gmx.ch>
2022-01-23 15:17:40 +0100

    ghost images for mouse

--- src/draw.py
+++ src/draw.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame import HWSURFACE, DOUBLEBUF, RESIZABLE, QUIT
 
-from world import load_assets, blits
+from world import load_assets, blits, View
 FPS = 60
 
 assets = {}
@@ -9,10 +9,9 @@ assets = {}
 def main():
     pygame.init()
     pygame.display.set_caption(__file__)
-    screen = pygame.display.set_mode((240,180), HWSURFACE|DOUBLEBUF|RESIZABLE)
+    screen = pygame.display.set_mode((800,600), HWSURFACE|DOUBLEBUF|RESIZABLE)
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("Arial", 18)
-
 
     def update_fps():
     	fps = str(int(clock.get_fps()))
@@ -23,12 +22,24 @@ def main():
 
     running = True
 
-    view_angle = 0
+    view = View(0, 400, 100)
+
+    drag_start_coord = None
 
     while running:
         screen.fill((148, 179, 167))
-        screen.blits(blit_sequence=blits(view_angle), doreturn=False)
+        screen.blits(blit_sequence=blits(view, pygame.mouse.get_pos()), doreturn=False)
         screen.blit(update_fps(), (10,0))
+
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]:
+            view.x_offset -= 5
+        if pressed[pygame.K_RIGHT]:
+            view.x_offset += 5
+        if pressed[pygame.K_UP]:
+            view.y_offset -= 5
+        if pressed[pygame.K_DOWN]:
+            view.y_offset += 5
 
         pygame.display.flip()
         for event in pygame.event.get():
@@ -38,23 +49,21 @@ def main():
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
             elif event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_RETURN:
-                    view_angle = (view_angle + 1) % 4
-        clock.tick(FPS*1000)
+                    w, h = screen.get_size()
+                    view.rotate(w/2, h/2)
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                if event.button == 2:
+                    drag_start_coord = event.pos
+            elif event.type==pygame.MOUSEBUTTONUP:
+                if event.button == 2:
+                    drag_start_coord = None
+            elif event.type==pygame.MOUSEMOTION:
+                if drag_start_coord:
+                    view.x_offset += event.pos[0] - drag_start_coord[0]
+                    view.y_offset += event.pos[1] - drag_start_coord[1]
+                    drag_start_coord = event.pos
 
-# def load_assets():
-#     assets['ball'] = pygame.image.load('art/ball2.png').convert_alpha()
-#
-# balls = None
-# def blits():
-#     import random
-#     global balls
-#     if True or balls is None:
-#         balls = [
-#             (assets['ball'], (random.randrange(2000), random.randrange(1000)))
-#             for _ in range(1000)
-#         ]
-#     return balls
-
+        clock.tick(FPS)
 
 if __name__ == '__main__':
     main()
--- src/world.py
+++ src/world.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import pygame
 
 TILE_WIDTH = 50
@@ -33,26 +34,80 @@ TILES = {
 }
 
 Map = [  # S -----> N       W v E
-    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
-    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
-    [('Grass', 0),   ('GrassSW', 0), ('GrassW', 0),  ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
-    [('Grass', 0),   ('GrassS', 0),  ('Grass', 1),   ('GrassW', 0),  ('GrassNW', 0), ('Grass', 0),   ],
-    [('Grass', 0),   ('GrassS', 0),  ('Grass', 1),   ('Grass', 1),   ('GrassN', 0),  ('Grass', 0),   ],
-    [('Grass', 0),   ('GrassSE', 0), ('GrassE', 0),  ('GrassE', 0),  ('GrassNE', 0), ('Grass', 0),   ],
-    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
-    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
+    [('Grass', -1),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ],
+    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('GrassW', 0),   ],
+    [('Grass', 0),   ('GrassSW', 0), ('GrassW', 0),  ('Grass', 0),   ('Grass', 0),   ('GrassW', 1),   ],
+    [('Grass', 0),   ('GrassS', 0),  ('Grass', 1),   ('GrassW', 0),  ('GrassNW', 0), ('GrassW', 2),   ],
+    [('Grass', 0),   ('GrassS', 0),  ('Grass', 1),   ('Grass', 1),   ('GrassN', 0),  ('Grass', 3),   ],
+    [('Grass', 0),   ('GrassSE', 0), ('GrassE', 0),  ('GrassE', 0),  ('GrassNE', 0), ('GrassE', 2),   ],
+    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('GrassE', 1),   ],
+    [('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('Grass', 0),   ('GrassE', 0),   ],
 ]
 
 MAP_SIZE_NS = 6
 MAP_SIZE_EW = 8
+
+@dataclass
+class View:
+    angle: int
+    x_offset: int
+    y_offset: int
+
+    def en_from_uv(self,u,v):
+        if self.angle == VIEW_FROM_SW:
+            e = MAP_SIZE_EW - 1 - u
+            n = MAP_SIZE_NS - 1 - v
+        elif self.angle == VIEW_FROM_NW:
+            e = MAP_SIZE_EW - 1 - v
+            n = u
+        elif self.angle == VIEW_FROM_NE:
+            e = u
+            n = v
+        elif self.angle == VIEW_FROM_SE:
+            e = v
+            n = MAP_SIZE_NS - 1 - u
+        return e,n
+
+    def uv_from_en(self,e,n):
+        if self.angle == VIEW_FROM_SW:
+            u = MAP_SIZE_EW - 1 - e
+            v = MAP_SIZE_NS - 1 - n
+        elif self.angle == VIEW_FROM_NW:
+            v = MAP_SIZE_EW - 1 - e
+            u = n
+        elif self.angle == VIEW_FROM_NE:
+            u = e
+            v = n
+        elif self.angle == VIEW_FROM_SE:
+            v = e
+            u = MAP_SIZE_NS - 1 - n
+        return u,v
+
+    def rotate(self, cx, cy):
+         u = (2 * cy - cx - 2*self.y_offset + self.x_offset)/(2*TILE_WIDTH)
+         v = (2 * cy + cx - 2*self.y_offset - self.x_offset)/(2*TILE_WIDTH)
+
+         e, n = self.en_from_uv(u, v)
+         print((u,v), (e,n))
+         self.angle = (self.angle + 1) % 4
+         u, v = self.uv_from_en(e, n)
+
+         self.x_offset = int(cx - TILE_WIDTH*(v-u))
+         self.y_offset = int(cy - TILE_WIDTH*(u+v)/2)
 
 def load_assets():
     global images
     images = { name: (pygame.image.load(fname).convert_alpha(), dx, dy)
         for (name, (fname, dx, dy)) in IMAGES.items()}
 
-def blits(view_angle):
-    if view_angle == VIEW_FROM_SW or view_angle == VIEW_FROM_NE:
+def blits(view: View, sel_pos):
+    if sel_pos is not None:
+        sel_x, sel_y = sel_pos
+    else:
+        sel_x = sel_y = -1
+
+    selected_blit = None
+    if view.angle == VIEW_FROM_SW or view.angle == VIEW_FROM_NE:
         U = MAP_SIZE_EW
         V = MAP_SIZE_NS
     else:
@@ -60,19 +115,21 @@ def blits(view_angle):
         V = MAP_SIZE_EW
     for u in range(U):
         for v in range(V):
-            if view_angle == VIEW_FROM_SW:
-                e = MAP_SIZE_EW - 1 - u
-                n = MAP_SIZE_NS - 1 - v
-            elif view_angle == VIEW_FROM_NW:
-                e = MAP_SIZE_EW - 1 - v
-                n = u
-            elif view_angle == VIEW_FROM_NE:
-                e = u
-                n = v
-            elif view_angle == VIEW_FROM_SE:
-                e = v
-                n = MAP_SIZE_NS - 1 - u
-
+            e,n = view.en_from_uv(u,v)
             tile, h = Map[e][n]
-            surf, dx, dy = images[TILES[tile][view_angle]]
-            yield surf, (500+TILE_WIDTH*(v-u)-dx, 100+TILE_WIDTH*(u+v)//2-h*Z_OFFSET-dy)
+            surf, dx, dy = images[TILES[tile][view.angle]]
+            x = view.x_offset + TILE_WIDTH * (v-u) - dx
+            y = view.y_offset + TILE_WIDTH * (u+v)//2 - h*Z_OFFSET - dy
+            sw, sh = surf.get_size()
+            if sel_pos is not None and x <= sel_x < x+sw and y <= sel_y < y+sh:
+                pixel = surf.get_at((sel_x - x, sel_y - y))
+                if pixel[3]:
+                    # not completely transparent
+                    selected_blit = (surf, x, y)
+            yield surf, (x, y)
+    if selected_blit:
+        surf, x, y = selected_blit
+        ghost = surf.copy()
+        ghost.fill((128,128,128), special_flags=pygame.BLEND_MULT)
+        ghost.fill((128,128,128), special_flags=pygame.BLEND_ADD)
+        yield ghost, (x, y)
