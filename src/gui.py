6e280d21f5b9c2b2260112891215b987258243ff
leovt <leonhard.vogt@gmx.ch>
2022-03-01 11:25:06 +0100

    modified gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,96 +1,99 @@
 import pygame
 
-
-class Grid:
-    def __init__(self, parent, rows, cols, sticky):
-        if isinstance(rows, int):
-            self.rows = (1,) * rows
-        else:
-            self.rows = tuple(rows)
-
-        if isinstance(cols, int):
-            self.cols = (1,) * cols
-        else:
-            self.cols = tuple(cols)
-
-        self.children = Grid._DummyDict()
-        self.sticky = set(sticky.lower())
-        assert self.sticky <= set('nwes')
+class Widget:
+    def __init__(self, parent):
         self.parent = parent
+        self.left = 0
+        self.top = 0
         self.width = 0
         self.height = 0
+        self.children = []
+        if parent is not None:
+            parent.register(self)
 
-    class _DummyDict(dict):
-        class Dummy:
-            def min_width(self):
-                return 0
-            def min_height(self):
-                return 0
-        dummy = Dummy()
-        def __missing__(self, key):
-            return self.dummy
+    def register(self, child):
+        self.children.append(child)
+
+
+from collections import namedtuple
+ChildRef = namedtuple('ChildRef', 'row,col,widget,sticky')
+
+class Frame(Widget):
+    def __init__(self, parent, rows, cols, fixed_width=0, fixed_height=0, pad_left=0, pad_top=0, pad_right=0, pad_bottom=0):
+        if isinstance(rows, int):
+            self.row_weights = (1,) * rows
+        else:
+            self.row_weights = tuple(rows)
+
+        if isinstance(cols, int):
+            self.col_weights = (1,) * cols
+        else:
+            self.col_weights = tuple(cols)
+
+        self.fixed_width = fixed_width
+        self.fixed_height = fixed_height
+
+        self.rows = [list() for _ in self.row_weights]
+        self.cols = [list() for _ in self.col_weights]
+        self.managed_children = []
+
+        Widget.__init__(self, parent)
+
+    def manage_child_pos(self, row, col, widget, sticky=''):
+        child = ChildRef(row, col, widget, sticky)
+        self.rows[row].append(child)
+        self.cols[col].append(child)
+        self.managed_children.append(child)
+        self.layout(self.width, self.height)
 
     def min_width(self):
+        if self.fixed_width:
+            return self.fixed_width
         return sum(self.min_colwidths())
 
     def min_height(self):
+        if self.fixed_height:
+            return self.fixed_height
         return sum(self.min_rowheights())
 
     def min_colwidths(self):
-        print([(k,c.min_width()) for (k,c) in self.children.items()])
-        return [max(self.children[i,j].min_width()
-                       for i, w_row in enumerate(self.rows))
-                   for j, w_col in enumerate(self.cols)]
+        return [max((x.widget.min_width() for x in row), default=0) for row in self.rows]
 
     def min_rowheights(self):
-        return [max(self.children[i,j].min_height()
-                       for j, w_col in enumerate(self.cols))
-                   for i, w_row in enumerate(self.rows)]
+        return [max((x.widget.min_height() for x in col), default=0) for col in self.cols]
 
     def layout(self, width, height):
-        tot_weigth = sum(self.cols)
+        if self.fixed_width:
+            self.width = min(self.fixed_width, width)
+        else:
+            self.width = width
+
+        tot_weight = sum(self.col_weights)
         min_col = self.min_colwidths()
-        delta = width - sum(min_col)
+        delta = self.width - sum(min_col)
         if delta < 0:
             self.col_widths = min_col
-            self.width = width
-        elif 'w' in self.sticky and 'e' in self.sticky:
-            self.col_widths = [m + delta/tot_weigth*w for (m,w) in zip(min_col, self.cols)]
-            print(self.cols, min_col, delta, tot_weigth, self.col_widths)
-            self.width = width
         else:
-            self.col_widths = min_col
-            self.width = sum(min_col)
+            self.col_widths = [m + delta/tot_weight*w for (m,w) in zip(min_col, self.col_weights)]
 
-        tot_weigth = sum(self.rows)
+        if self.fixed_height:
+            self.height = min(self.fixed_height, height)
+        else:
+            self.height = height
+        tot_weight = sum(self.row_weights)
         min_row = self.min_rowheights()
-        delta = height - sum(min_row)
+        delta = self.height - sum(min_row)
         if delta < 0:
             self.row_heights = min_row
-            self.height = height
-        elif 's' in self.sticky and 'n' in self.sticky:
-            self.row_heights = [m + delta/tot_weigth*w for (m,w) in zip(min_row, self.rows)]
-            self.height = height
         else:
-            self.row_heights = min_row
-            self.height = sum(min_height)
+            self.row_heights = [m + delta/tot_weight*w for (m,w) in zip(min_row, self.row_weights)]
 
-        for (i,j), child in self.children.items():
-            child.layout(self.col_widths[j], self.row_heights[i])
-
-
-    def child_resize(self):
-        if self.parent:
-            self.parent.child_resize()
-        else:
-            self.layout(self.width, self.height)
-
-    def draw(self, surf):
-        for (i,j), child in self.children.items():
+        for child in self.managed_children:
+            i, j = child.row, child.col
             left = sum(self.col_widths[:j])
             top = sum(self.row_heights[:i])
-            width = child.min_width()
-            height = child.min_height()
+            width = child.widget.min_width()
+            height = child.widget.min_height()
 
             if 'e' in child.sticky and 'w' in child.sticky:
                 width = self.col_widths[j]
@@ -110,20 +113,30 @@ class Grid:
             else:
                 top += (self.row_heights[i] - height) // 2
 
-            rect = pygame.Rect(left, top, width, height)
+            child.widget.left = left
+            child.widget.top = top
+            child.widget.width = width
+            child.widget.height = height
+            try:
+                child.widget.layout(self.col_widths[child.col], self.row_heights[child.row])
+            except AttributeError:
+                pass
+
+    def draw(self, surf, offset_x, offset_y):
+        for child in self.children:
+            rect = pygame.Rect(child.left - offset_x, child.top - offset_y, child.width, child.height)
             rect = rect.clip(surf.get_rect())
             if rect.width and rect.height:
                 subsurf = surf.subsurface(rect)
-                child.draw(subsurf)
+                child.draw(subsurf, rect.left-child.left, rect.top-child.top)
 
-class Button:
-    def __init__(self, parent, text, font, sticky):
+class Button(Widget):
+    def __init__(self, parent, text, font):
+        Widget.__init__(self, parent)
         self.text = text
         self.text_width, self.text_height = font.size(text)
         self.font = font
-        self.sticky = set(sticky.lower())
         self.pressed = False
-        assert self.sticky <= set('nwes')
 
     def min_width(self):
         return self.text_width + 10
@@ -134,7 +147,7 @@ class Button:
     def layout(self, width, height):
         pass
 
-    def draw(self, surf):
+    def draw(self, surf, offset_x, offset_y):
         w, h = surf.get_size()
 
         if self.pressed:
@@ -149,25 +162,23 @@ class Button:
             ((w - self.text_width)//2, (h - self.text_height)//2))
 
 
-class TopWindow(Grid):
-    def __init__(self, text, font, left, top, width, height):
+class TopWindow(Frame):
+    def __init__(self, parent, text, font, cols, rows, left, top, width, height):
+        self.text_width, self.text_height = font.size(text)
+        Frame.__init__(self, parent, cols, rows, pad_top = self.text_height)
         self.left = left
         self.top = top
         self.width = width
         self.height = height
         self.text = text
-        self.text_width, self.text_height = font.size(text)
         self.font = font
         self.drag_pos = None
 
-    def draw(self, surf):
-        rect = pygame.Rect(self.left, self.top, self.width, self.height)
-        rect = rect.clip(surf.get_rect())
-        if rect.width and rect.height:
-            sub = surf.subsurface(rect)
-            sub.fill((128,112,144))
-            sub.blit(self.font.render(self.text, 1, pygame.Color("black")),
-            ((self.width - self.text_width - self.text_height)//2, 0))
+    def draw(self, surf, offset_x, offset_y):
+        surf.fill((128,112,144))
+        surf.blit(self.font.render(self.text, 1, pygame.Color("black")),
+            ((self.width - self.text_width - self.text_height)//2 - offset_x, -offset_y))
+        Frame.draw(self, surf, offset_x, offset_y)
 
     def on_mouse_down(self, event):
         self.drag_pos = event.pos
@@ -193,27 +204,31 @@ def main():
     font = pygame.font.SysFont("Arial", 18)
     clock = pygame.time.Clock()
 
-    top = Grid(None, 3, 3, 'nwes')
+    root = Frame(None, 3, 3)
 
-    btn1 = Button(top, 'hello', font, 'nwes')
-    btn2 = Button(top, 'hello, world!', font, 's')
-    btn3 = Button(top, 'hi', font, 'ns')
+    btn1 = Button(root, 'hello', font)
+    btn2 = Button(root, 'hello, world!', font)
+    btn3 = Button(root, 'hi', font)
     btn2.pressed = True
 
-    tw = TopWindow('Window Title', font, 231, 111, 300, 200)
+    root.manage_child_pos(2, 0, btn1, 'nwes')
+    root.manage_child_pos(2, 1, btn2, 's')
+    root.manage_child_pos(1, 2, btn3, 'ns')
 
-    top.children[2,0] = btn1
-    top.children[2,1] = btn2
-    top.children[1,2] = btn3
-    top.layout(800, 600)
+    tw = TopWindow(root, 'Window Title', font, 1, 1, 231, 111, 300, 200)
+
+    btn4 = Button(tw, 'Button 4', font)
+    tw.manage_child_pos(0,0,btn4,'')
+
+    assert tw in root.children
+    root.layout(800, 600)
 
     running = True
     frame = 0
     while running:
         frame += 1
         screen.fill((148, 179, 167))
-        top.draw(screen)
-        tw.draw(screen)
+        root.draw(screen, 0, 0)
 
         pygame.display.flip()
         for event in pygame.event.get():
@@ -221,7 +236,7 @@ def main():
                 running = False
             elif event.type == pygame.VIDEORESIZE:
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-                top.layout(*screen.get_size())
+                root.layout(*screen.get_size())
             elif event.type in (pygame.MOUSEBUTTONDOWN, pygame.MOUSEMOTION, pygame.MOUSEBUTTONUP):
                 if tw.left <= event.pos[0] < tw.left + tw.width and \
                    tw.top <= event.pos[1] < tw.top + tw.height:
